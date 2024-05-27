7aa3e4bc0bb900942c61399163320d07c1c4c43d
Prakash Patil <prakashkumarmeti@gmail.com>
2024-05-27 15:26:28 +0530

    update config file

---
+++ app.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello, CircleCI!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_calculator.py
@@ -0,0 +1,19 @@
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+
+def test_sub():
+    assert sub(5, 3) == 2
+    assert sub(1, 1) == 0
+    assert sub(-1, 1) == -2
+    assert sub(3, 3) != 2
